8fb5df27eb9a4d1b5211bd39911305c1104878c7
RayhanalKavey <rayhanalkavey@gmail.com>
2023-05-31 19:28:03 +0600

    Database connection and listening on port successfully.

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+DbConnection();
+async function DbConnection() {
+  try {
+    // await mongoose.connect(config.database_url as string);
+
+    await mongoose.connect(`${config.database_url}`);
+    console.log(" 🛢️ Database connected successfully ! 🔥");
+
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log("❌ Failed to connect database 🛢️");
+  }
+}
